3509b0d32e8f095b010c97c847293f1eb78c2a66
randyzwitch <rzwitch@gmail.com>
2017-12-12 11:26:37 -0500

    Travis authentication part 2

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using Twitter, Base.Test
 
-twitterauth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+twitterauth(ENV["CONSUMER_KEY"], ENV["CONSUMER_SECRET"], ENV["ACCESS_TOKEN"], ENV["ACCESS_TOKEN_SECRET"])
 
 mentions_timeline_default = get_mentions_timeline()
 @test 0 <= length(mentions_timeline_default) <= 20
