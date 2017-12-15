3db16845d5a0834747958beebf51d5d5df058a4b
randyzwitch <rzwitch@gmail.com>
2017-12-15 11:13:52 -0500

    Type cleanup, more tests
    - Add tests
    - Improve types
    - Change authentication keys
    - Clean up README

--- src/Twitter.jl
+++ src/Twitter.jl
@@ -1,4 +1,4 @@
-__precompile__(true)
+__precompile__()
 
 module Twitter
 
--- src/general.jl
+++ src/general.jl
@@ -2,13 +2,13 @@
 #TODO: Figure out how to do OAuth authentication directly, rather than user putting in credentials directly
 function twitterauth(consumer_key::String, consumer_secret::String, oauth_token::String, oauth_secret::String)
     #Create a global variable to hold return from this function
-    global twittercred
+    global TWITTERCRED
 
-    return twittercred = TWCredential(consumer_key, consumer_secret, oauth_token, oauth_secret)
+    return TWITTERCRED = TWCredential(consumer_key, consumer_secret, oauth_token, oauth_secret)
 
 end
 
 #General functions for OAuth authenticated GET/POST request
-get_oauth(endpoint::String, options::Dict) = oauth_request_resource(endpoint, "GET", options, twittercred.consumer_key, twittercred.consumer_secret, twittercred.oauth_token, twittercred.oauth_secret)
+get_oauth(endpoint::String, options::Dict) = oauth_request_resource(endpoint, "GET", options, TWITTERCRED.consumer_key, TWITTERCRED.consumer_secret, TWITTERCRED.oauth_token, TWITTERCRED.oauth_secret)
 
-post_oauth(endpoint::String, options::Dict) = oauth_request_resource(endpoint, "POST", options, twittercred.consumer_key, twittercred.consumer_secret, twittercred.oauth_token, twittercred.oauth_secret)
+post_oauth(endpoint::String, options::Dict) = oauth_request_resource(endpoint, "POST", options, TWITTERCRED.consumer_key, TWITTERCRED.consumer_secret, TWITTERCRED.oauth_token, TWITTERCRED.oauth_secret)
